6e1a92437aa6f1fdb65c97e0eae4e299a467d7e5
JohnnySprit <flanegt@gmail.com>
2026-08-01 17:36:35 -0400

    initial Spring Boot struct

---
+++ src/test/java/dev/jnzheng/meridian/FolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jnzheng.meridian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
